6194eff32ed9c0cc5a1a621d35c457eb5b32a5e6
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2021-01-17 18:32:48 -0400

    Using component AppButton

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
 <template>
   <div id="app">
-    [place your component here]
+    <div>
+      <AppButton label="Primary" variant="primary"/>
+    </div>
+    <div>
+      <AppButton label="Secondary" variant="secondary"/>
+    </div>
+    <div>
+      <AppButton label="disabled" :disabled="true"/>
+    </div>
+    <div>
+      <AppButton iconName="home"/>
+    </div>
+    <div>
+      <AppButton>
+        <i class="fa fa-video-camera" style="margin-right: 2px"/>
+        <span>Slot</span>
+      </AppButton>
+    </div>
+    <div>
+      <AppButton label="Both" icon-name="home" variant="primary"/>
+    </div>
   </div>
 </template>
 
 <script>
 
+import AppButton from './components/button/AppButton.vue';
+
 export default {
   name: 'App',
+  components: { AppButton },
 };
 </script>
+<style scoped>
+div {
+  margin-bottom: 5px;
+}
+</style>
