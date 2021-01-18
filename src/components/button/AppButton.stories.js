1ec92d6008970187affbe1db170a5b74c8ad8060
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2021-01-18 00:07:08 -0400

    Adding options in the storybook

--- src/components/button/AppButton.stories.js
+++ src/components/button/AppButton.stories.js
@@ -4,25 +4,48 @@ export default {
   title: 'Components/Button',
 };
 
-export const Simple = () => ({
-  render() {
-    return <AppButton label="Simple"/>;
+export const Simple = (args, { argTypes }) => ({
+  components: { AppButton },
+  template: '<AppButton  label="Simple" :variant="variant" :disabled="disabled"/>',
+  props: Object.keys(argTypes),
+  data() {
+    return args;
   },
 });
+Simple.argTypes = {
+  variant: {
+    control: {
+      type: 'select',
+      options: [
+        'primary',
+        'secondary',
+      ],
+    },
+  },
+  disabled: {
+    control: {
+      type: 'select',
+      options: [
+        true,
+        false,
+      ],
+    },
+  },
+};
 
 export const Primary = () => ({
   components: { AppButton },
-  template: '<AppButton  label="Primary" button-variant="primary"/>',
+  template: '<AppButton  label="Primary" variant="primary"/>',
 });
 
 export const Secondary = () => ({
   components: { AppButton },
-  template: '<AppButton  label="Secondary" button-variant="secondary"/>',
+  template: '<AppButton  label="Secondary" variant="secondary"/>',
 });
 
 export const Disabled = () => ({
   components: { AppButton },
-  template: '<AppButton  label="Disabled" :disable="true" />',
+  template: '<AppButton  label="Disabled" :disabled="true" />',
 });
 export const Icon = () => ({
   components: { AppButton },
